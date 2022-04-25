65d69e298a7b6c2e984078ee3cd787c7e353a97b
Paul <skrimorill@mail.ru>
2022-04-25 10:14:28 +0300

    task2.1 solved

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0
+  for(let key in salaries) {
+    if(typeof salaries[key] !== 'number' || isNaN(salaries[key]) || salaries[key] == 'Infinity' || salaries[key] == '-Infinity') {
+      sum = sum
+    }
+    else if(typeof salaries[key] == 'number') {
+      sum += salaries[key]
+    }
+  }
+  return sum
 }
+ 
