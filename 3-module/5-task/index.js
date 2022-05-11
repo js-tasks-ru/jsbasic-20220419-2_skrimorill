fd94f160464ba31458e85fbfd089cae9cc83c06b
Paul <skrimorill@mail.ru>
2022-05-11 07:20:18 +0300

    task3.5 solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+let arr =  str.split(' ').filter(elem => !isNaN(elem))
+let result = {
+  min: Math.min(...arr),
+  max: Math.max(...arr),
+}
+return result
 }
